7222d1b643154baf8e1a58bae5b00ca4dcdda7d1
Akon Zen <zxk44944@live.com>
2023-07-20 02:43:01 +0000

    setup playground

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { TanStackQueryGetStarted } from './learn/tanstack-query/get-started';
+import { TanStackQueryGetStarted } from "./learn/tanstack-query/get-started";
+import { useState } from "react";
 
 export default function App() {
+  const [it, setIt] = useState(0);
   return (
-    <>
-      <TanStackQueryGetStarted />
-    </>
+    <div key={it}>
+      <section>
+        <header>
+          <h1>Playground</h1>
+          <button onClick={() => setIt(Date.now())}>click to refresh</button>
+          <div style={{ margin: "20px 0", border: "1px solid grey" }}></div>
+        </header>
+      </section>
+      <main>
+        {/* demos here */}
+        <TanStackQueryGetStarted />
+      </main>
+    </div>
   );
 }
